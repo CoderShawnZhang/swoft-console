1421bbef11b3e8fec0efa5f044423f60c1201a6c
zhanghongbo <taobao1875@163.com>
2019-06-01 16:23:33 +0800

    init

---
+++ src/Console.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghongbo
+ * Date: 2019/6/1
+ * Time: 下午4:20
+ */
+namespace SwoftRewrite\Console;
+
+class Console
+{
+    public function test()
+    {
+        
+    }
+}
